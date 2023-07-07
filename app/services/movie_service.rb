e147a1c3ceb02b4e410d8865cd8801ca58360765
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-07 14:58:21 -0600

    Feat: Add to MovieService

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,12 +1,20 @@
 class MovieService
-  def self.conn
-    Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
-      faraday.params["api_key"] = ENV["MOVIE_API_KEY"]
-    end
+  def top_rated_movies
+    get_url("movie/top_rated")
+  end
+
+  def search_movies(search)
+    get_url("search/movie?query=#{search}")
   end
 
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
+      faraday.params["api_key"] = ENV["MOVIE_API_KEY"]
+    end
   end
 end
